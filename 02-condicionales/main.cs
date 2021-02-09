d06d62b22fe915cbcebd1275c83aec5a8d82568c
Johnnie Tirado <tiradoyamin@gmail.com>
2021-02-09 02:44:12 +0000

    Finished class

--- 02-condicionales/main.cs
+++ 02-condicionales/main.cs
@@ -3,7 +3,25 @@ using System;
 namespace Codealo {
   class Program {
     public static void Main(string[] args) {
-      Console.WriteLine("Hello World");
+      // Verdadera o Falsa
+
+      int vida = 0;
+      int dmg = 0;
+
+      // == Igual a
+      // != No Es igual
+      // > Mas grande que
+      // < Mas pequeño que
+      // && Y (And)
+      // || O (Or)
+
+      if(vida > 0 && dmg < vida) {
+        Console.WriteLine("Estas vivo");
+      } else {
+        Console.WriteLine("Estas muerto... 💀");
+      }
+
+      
     }
   }
 }
